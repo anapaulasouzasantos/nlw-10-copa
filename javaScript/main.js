342ce827010b11220a48d5f4fdd382be7812c2fb
Ana Paula Souza dos Santos <anapaulasouza2095@gmail.com>
2022-11-08 12:32:40 -0300

    add jogos

--- javaScript/main.js
+++ javaScript/main.js
@@ -26,6 +26,8 @@ function createCard(date, day, games) {
 }
 
 document.querySelector('#cards').innerHTML =
+    createCard('20/11', 'domingo', createGame('qatar', '13:00', 'ecuador')) +
+    createCard('21/11', 'segunda', createGame('england', '10:00', 'iran') + createGame('senegal', '13:00', 'netherlands')) +
     createCard('24/11', 'quinta', createGame('switzerland', '07:00', 'cameroon') + createGame('uruguay', '10:00', 'south-korea') + createGame('portugal', '13:00', 'ghana') + createGame('brazil', '16:00', 'serbia')) +
     createCard('28/11', 'segunda', createGame('switzerland', '13:00', 'brazil')) +
     createCard('02/12', 'sexta', createGame('brazil', '16:00', 'cameroon'))
